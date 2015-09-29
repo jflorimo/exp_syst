73a4e96bbf5d387c979d886a4efd1a2e720aa934
Dany SOUSA <dsousa@e2r4p21.42.fr>
2015-09-29 11:46:24 +0200

    [INIT] 2

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+
+def parse( lines ):
+	for tmp in lines:
+		print(tmp)
+		# Go parse here
+
+def read( filename ):
+	result = []
+
+	try:
+		fd = open(filename, 'r')
+	except Exception:
+		print("[ERROR] Can't open specified file")
+		exit(-2)
+
+	for tmp in fd :
+		result.append(tmp)
+
+	parse(result)
+
+if ( len(sys.argv) < 2 ) :
+	print("Usage : ./" + sys.argv[0] + " [Input file]")
+	exit( -1 )
+
+print(sys.argv[1])
+read(sys.argv[1])
