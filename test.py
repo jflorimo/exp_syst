269c74a4eb64e28d2f808bd31913b4947222d337
Dany SOUSA <dsousa@e2r4p21.42.fr>
2015-10-01 14:22:01 +0200

    [TMP]

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ B = Var('B')
 C = Var('C')
 
 A.setValue(1)
-B.setValue(1)
+B.setValue(0)
 C.setValue(0)
 
 varMap = {
@@ -24,5 +24,21 @@ tmp = query
 for (name, value) in varMap.items():
 	tmp = str.replace(tmp, name, str(value.getValue()))
 print(tmp)
-print(str(test.run(varMap)))
+result = test.run(varMap)
+print("result brut = " + str(result))
 
+
+def interpretResult( right, search, result ):
+	undetermined = "!?[A-Z][|^]"
+	composite = "!?[A-Z]\+"
+	if ( re.search(undetermined, right) != None ):
+		return ( -1 )
+	elif ( re.search(composite, right) != None and result != 1 ):
+		return ( -1 )
+	elif (right.find( "!" + search ) != -1 and result != -1):
+		return ( 0 if result == 1 else 1 )
+	return (result)
+
+
+right = "!X + Y"
+print( "result for X in =>" + right + " : " + str(interpretResult(right, "X", result)) )
